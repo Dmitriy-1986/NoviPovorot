199525eab616c41a88ba371038536c3bf1ae07c1
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2021-10-10 23:40:05 +0300

    Update signInSiteMap.js

--- scripts/signInSiteMap.js
+++ scripts/signInSiteMap.js
@@ -1,10 +1,8 @@
-const login = prompt('Введите login');
-const password = prompt('Введите password');
-if (login == "admin" && password == "1111") {
-    //if () {
+const password = prompt('Введите пароль');
+if (password == "1111") {
+    
          window.onload;
-     // alert('Приветствую на карте сайта!');
-    // }
+ 
 } else {
   
       window.location.href = 'index.html';

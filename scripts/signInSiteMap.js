9a71a1c46cd401764529d9fc9cc7167c63fd14a8
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2021-10-10 17:13:13 +0300

    Update signInSiteMap.js

--- scripts/signInSiteMap.js
+++ scripts/signInSiteMap.js
@@ -1,1 +1,11 @@
+const name = prompt('Введите пароль');
 
+if (name == "1111") {
+  
+      alert('Приветствую на карте сайта!');
+  
+} else {
+  
+      window.location.href = 'index.html';
+  
+}

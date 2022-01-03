4f07821e72fa941e5c58746672f6ad6b245b78e2
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-01-03 21:29:50 +0200

    Update allTours.js

--- scripts/allTours.js
+++ scripts/allTours.js
@@ -1,5 +1,23 @@
  let allTours = [
-		{link: 'https://novipovorot.pp.ua/page-Dzharylgach.html', linkName: '1Джарылгач - Украинские Мальдивы'}
+	 {link: ' ', linkName: ' '},
+	 {link: ' ', linkName: ' '},
+	 {link: ' ', linkName: ' '},
+	 {link: ' ', linkName: ' '},
+	 {link: ' ', linkName: ' '},
+	 {link: ' ', linkName: ' '},
+	 {link: ' ', linkName: ' '},
+	 {link: ' ', linkName: ' '},
+	 {link: ' ', linkName: ' '},
+	 {link: 'https://novipovorot.pp.ua/page-cofe-Isla.html', linkName: '50 Оттенков Кофе Isla'},
+	 {link: 'https://novipovorot.pp.ua/page-Stambul.html', linkName: 'Стамбул - Столица Тюльпанов'},
+	 {link: 'https://novipovorot.pp.ua/page-lions-and-horses.html', linkName: 'Львята И Лошадки'},
+	 {link: 'https://novipovorot.pp.ua/page-Dzharylgach.html', linkName: 'Джарылгач - Украинские Мальдивы'},
+	 {link: 'https://novipovorot.pp.ua/page-TicToc-pati.html', linkName: 'Tik Tok Pati'},
+	 {link: 'https://novipovorot.pp.ua/page-gold-mines.html', linkName: 'Золотые Прииски Закарпатья'},
+	 {link: 'https://novipovorot.pp.ua/page-eight-march.html', linkName: 'Букет К 8 Марта'},
+	 {link: 'https://novipovorot.pp.ua/page-Odessa-on-march.html', linkName: 'Одесса'},
+	 {link: 'https://novipovorot.pp.ua/page-LOL.html', linkName: 'ЛОЛ'},
+	 {link: 'https://novipovorot.pp.ua/page-Limonariy.html', linkName: 'Лимонарий'}
 ];
 		
  for(var i = 0; i < allTours.length; i++) {

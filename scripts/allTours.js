09bf6fe78bd0d2a91e58e36abfde456580e48052
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-01-03 21:07:12 +0200

    Update allTours.js

--- scripts/allTours.js
+++ scripts/allTours.js
@@ -1,1 +1,9 @@
-
+ let allTours = [
+		{link: 'https://novipovorot.pp.ua/page-Dzharylgach.html', linkName: '1Джарылгач - Украинские Мальдивы'}
+];
+		
+ for(var i = 0; i < allTours.length; i++) {
+     document.write(
+       '<a class="dropdown-item" href="' + allTours[i].link + '">' + allTours[i].linkName + '</a>'
+     );
+}
